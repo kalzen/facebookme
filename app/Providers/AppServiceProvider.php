78d46b41e5fe688b067aecbbc4221cc657c272a5
kalzen <manhto109@gmail.com>
2025-03-01 23:49:22 +0700

    xoá phần thừa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,10 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
-        }
-        
         Paginator::useBootstrap();
     }
 }
